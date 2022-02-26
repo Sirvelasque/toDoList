30da16abbd08712b2351266b3fd75ae73d66bb31
Sirvelasque <velasquezwalter10@yahoo.es>
2022-02-26 10:47:16 -0600

    set base html and DOM

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import _ from 'lodash';
+import _, { create } from 'lodash';
 import './style.css';
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- element.classList.add('hello');
+let tareas = [];
+const container = document.getElementById('list');
 
-  return element;
-}
-
-document.body.appendChild(component());
+const defaultTasks = [
+  {
+    task: 'study'
+  },
+  {
+    task: 'eat'
+  },
+  {
+    task: 'sleep'
+  },
+];
