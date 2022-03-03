8cda78920055f66007f41be98fc7c65ef708fca8
Sirvelasque <velasquezwalter10@yahoo.es>
2022-03-03 14:15:33 -0600

    Revert "Merge pull request #1 from Sirvelasque/List_estructure"
    This reverts commit 6178f06dd463485d5aaf81c078ecd486ce36dd4c, reversing
    changes made to 602623d08d690a33763f43d74695185285e294fe.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,13 @@
+import _ from 'lodash';
 import './style.css';
+function component() {
+  const element = document.createElement('div');
 
-class Task {
-  constructor(completed, task, index) {
-    this.completed = completed;
-    this.task = task;
-    this.index = index;
-  }
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ element.classList.add('hello');
+
+  return element;
 }
 
-const tasks = [];
-
-const defaultTasks = [
-  {
-    completed: false,
-    task: 'study',
-    index: 1,
-  },
-  {
-    completed: false,
-    task: 'eat',
-    index: 2,
-  },
-  {
-    completed: false,
-    task: 'sleep',
-    index: 3,
-  },
-];
-
-function addTask(task) {
-  const container = document.getElementById('list');
-  const row = document.createElement('li');
-  row.innerHTML = `
-  <input name="completed" type="checkbox" class="completed-sheck">
-  <div>${task.task}</div>
-  `;
-  container.appendChild(row);
-
-  const taskitem = new Task(task.completed, task.task, task.index);
-  tasks.push(taskitem);
-}
-
-function displayTasks() {
-  defaultTasks.forEach((task) => addTask(task));
-}
-displayTasks();
+document.body.appendChild(component());
